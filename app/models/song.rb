6c53b3d52c4b18a1b1b37e883d841abfd0350a6a
Tim Escher <tim@eschers.com>
2016-04-28 14:44:47 -0800

    tweaks

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,10 +2,10 @@ class Song < ActiveRecord::Base
   require 'csv'
 
   def self.import(file)
-    count = 0;
-    CSV.foreach(file.path, headers:true, :row_sep => :auto, col_sep: "\t", encoding: 'windows-1251:utf-8') do |row|
-      pp row
-      Song.create! row.to_hash.map{ |k,v| {ColumnTransform.find_by_external(k).to_sym => v}}
+    count = 0
+    CSV.foreach(file.path, headers:true, :row_sep => :auto, col_sep: "\t", encoding: 'utf-8') do |row|
+      puts row.to_hash
+      Song.create! row.to_hash.transform_keys{ |k| ColumnTransform.find_by_external(k).internal.to_sym }
       count += 1
     end
     count
